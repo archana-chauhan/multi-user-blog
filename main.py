e8bccbe0b599db52712f768ec222145839a71b19
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2017-01-03 01:30:53 +0530

    Add post and comment datastore model

--- main.py
+++ main.py
@@ -120,38 +120,120 @@ class MainHandler(webapp2.RequestHandler):
         return cookie_val and check_secure_val(cookie_val)
 
 
+# User model
 class User(db.Model):
     '''
         User : This is Google Datastore kind that stores information
         about registered users.
 
         Properties :
+            name : this property store the name of the user
             username : this property store the unique username of the user
             password : this property store the encryted password of user
             email : thsi property store unique email of the user
     '''
+    name = db.StringProperty(required=True)
     username = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
     email = db.StringProperty(required=True)
 
     @classmethod
     def get_user_by_id(cls, user_id):
+        '''
+            get_user_by_id : function return user instance by id
+        '''
         return User.get_by_id(user_id)
 
     @classmethod
-    def get_user_by_name(cls, username):
+    def get_user_by_username(cls, username):
+        '''
+            get_user_by_name : function return user instance by username
+        '''
         return db.GqlQuery("SELECT * FROM User WHERE username= :username", username=username).get()     # NOQA
 
     @classmethod
-    def user_register(cls, username, password, email):
+    def user_register(cls, name, username, password, email):
+        '''
+            user_register : function creates hash of password then create data
+            entity of User(model) class
+        '''
         password_hash = make_pwd_hash(password)
-        return User(username=username, password=password_hash, email=email)
+        return User(name=name, username=username, password=password_hash,
+                    email=email)
 
     @classmethod
     def user_login(cls, username, password):
-        user = cls.get_user_by_name(username)
+        '''
+            user_login : function retrieve user instance by username,
+            then retrieve hashed password then match the hashed password by
+            the new hash created via username and password entered during
+            login. If it matches it returns true else return false
+        '''
+        user = cls.get_user_by_username(username)
         hashed_password = user.password
-        return valid_pwd(username, password, hashed_password) 
+        return valid_pwd(username, password, hashed_password)
+
+
+# Post model
+class Post(db.model):
+    '''
+        Post : This is Google Datastore kind that stores information
+        about posts posted by user.
+
+        Properties :
+            subject : this property store the subject of the post
+            content : this property store the content of the post
+            created_date_time : this property store the date and time
+                when the post was created
+            last_modified : this property store the data and time when
+                the post was last modified
+            created_by_user : this property store the data about the user
+                who created it
+            total_likes : this property store the total likes for the post
+            liked_by_users : this property stores the list of the users who
+                liked the post
+
+    '''
+    subject = db.StringProperty(required=True)
+    content = db.StringProperty(required=True)
+    created_date_time = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    created_by_user = db.StringProperty(required=False)
+    total_likes = db.IntegerProperty(required=False)
+    liked_by_users = db.ListProperty(str)
+
+    @classmethod
+    def get_post_by_username(cls, username):
+        posts = db.GqlQuery("SELECT * FROM Post WHERE created_by_user = :username ORDER BY created_date_time DESC", username=username)  # NOQA
+        return posts
+
+
+# Comment model
+class Comment(db.Model):
+    '''
+        Comment : This is Google Datastore kind that stores information
+        about comments posted by user in posts.
+
+        Properties :
+            comment : this property stores the comment content on the post
+            comment_by_user : this property store the user data who commented
+                on post
+            post_id : this property store the post id on which comment was made
+            created_date_time : this property store the date time when comment
+                was made
+            last_modified : this property store the date time when the comment
+                was last modified
+    '''
+    comment = db.TextProperty(required=True)
+    comment_by_user = db.StringProperty(required=True)
+    post_id = db.IntegerProperty(required=True)
+    created_date_time = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    @classmethod
+    def get_comments(cls, post_id):
+        comments = db.GqlQuery("SELECT * FROM Comment WHERE post_id= :post_id", post_id=int(post_id))   # NOQA
+        return comments
 
 
 class MainPage(webapp2.RequestHandler):
