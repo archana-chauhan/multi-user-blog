20b6dbc423c29b60eadcc724aa25ef780b4c3b56
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2017-01-02 19:01:45 +0530

    Add base.html for templates

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import os
 import webapp2
 import jinja2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello world')
+
+app = webapp2.WSGIApplication([
+      ('/',MainPage),
+      ], debug = True)
