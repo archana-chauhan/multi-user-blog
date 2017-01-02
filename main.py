1b1091bc96a198a363347ba468be0b02fcf303d0
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2017-01-02 23:25:08 +0530

    Add valid email,name,password check module

--- main.py
+++ main.py
@@ -1,12 +1,112 @@
 import os
+import re
+
 import webapp2
 import jinja2
+
+import hashlib
+import hmac
+
+import string
+import random
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+secret = 'du.uyX9fE~Tb6.pp&U3D-OsmYO,Gqi$^jS34tzu9'
+
+# Regular expression to validate username
+RE_USER = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+
+def valid_username(user_name):
+    '''
+        valid_username : function match the user name with the regular
+        expression. If matches return true else return false
+    '''
+    return user_name and RE_USER.match(user_name)
+
+
+# Regular expression to validate password
+RE_PASS = re.compile(r"^.{3,20}$")
+
+
+def valid_password(password):
+    '''
+        valid_password : function match the password with the regular
+        expression. If matches return true else return false
+    '''
+    return password and RE_PASS.match(password)
+
+# Regular expression to validate email address
+RE_EMAIL = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
+def valid_email(emailaddress):
+    '''
+        valid_email : function math the email with the regular expression.
+        If matches return true else return false
+    '''
+    return not email and RE_EMAIL.match(emailaddress)
+
+
+class MainHandler(webapp2.RequestHandler):
+    '''
+        This handler class contains generic functions that are inherited by
+        other child handler classes
+    '''
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class User(db.Model):
+    '''
+        User : This is Google Datastore kind that stores information
+        about registered users.
+
+        Properties :
+            username : this property store the unique username of the user
+            password : this property store the encryted password of user
+            email : thsi property store unique email of the user
+    '''
+    username = db.StringProperty(required=True)
+    password = db.StringProperty(required=True)
+    email = db.StringProperty(required=True)
+
+    @classmethod
+    def user_byid(cls, user_id):
+        return User.get_by_id(user_id)
+
+    @classmethod
+    def user_byname(cls, username):
+        return db.GqlQuery("SELECT * FROM User WHERE username= :username", username=username).get()     # NOQA
+
+    @classmethod
+    def user_register(cls, username, password, email):
+        return USer(username=username, password=password, email=email)
+
+
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello world')
 
+
 app = webapp2.WSGIApplication([
-      ('/',MainPage),
-      ], debug = True)
+      ('/', MainPage),
+      ('/login', HandlerLogin),
+      ('/logout', HandlerLogout),
+      ('/signup', HandlerSignUp)
+      ], debug=True)
